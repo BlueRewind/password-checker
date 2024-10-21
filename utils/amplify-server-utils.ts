12117c09db613ac616cc4d2a36b913c94a02f730
Aidan <aidan.alsaadoun@hotmail.co.uk>
2024-10-21 10:40:31 +0100

    push

--- utils/amplify-server-utils.ts
+++ utils/amplify-server-utils.ts
@@ -1,11 +1,10 @@
 import { authConfig } from "@/app/amplify-cognito-config";
-import { createServerRunner } from '@aws-amplify/adapter-nextjs'
 
-export const { runWithAmplifyServerContext } = createServerRunner({
-    config: {
-        Auth: authConfig
-    },
-});
+// export const { runWithAmplifyServerContext } = createServerRunner({
+//     config: {
+//         Auth: authConfig
+//     },
+// });
 
 import { signUp } from "aws-amplify/auth"
 
